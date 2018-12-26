76d4a9c6352d48ba81da156f22dc4beb2248de3a
= <tim.schaefer24@hotmail.de>
2018-12-26 22:39:44 +0100

    Read relative values

--- plotting_data.py
+++ plotting_data.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 f = open("msci.txt", "r")
 f = f.read()
 print(str(f))
-#print(f)
 y = json.loads(str(f))
 print(type(y))
 
@@ -21,16 +20,20 @@ dict_keys(['id', 'caption', 'benchmark_name', 'historicalData', 'historicalDataR
 '''
 
 # This is what i need
-print(y[0]["historicalData"][1])
+print(y[0]["historicalDataRelative"][1])
 
 portfolio_vals = [val["portfolio_return"] for val in y[0]["historicalData"]]
+portfolio_vals_rel = [val["portfolio_return"] for val in y[0]["historicalDataRelative"]]
 bench_vals = [val["benchmark_return"] for val in y[0]["historicalData"]]
+bench_vals_rel = [val["benchmark_return"] for val in y[0]["historicalDataRelative"]]
 date_keys = [datetime.datetime(val["date_year"], val["date_month"], 1) for val in y[0]["historicalData"]]
 
 
 main_df = pd.DataFrame(
     {'portfolio_vals': portfolio_vals,
+     #'portfolio_vals_rel' : portfolio_vals_rel,
      'bench_vals': bench_vals,
+    # 'bench_vals_rel': bench_vals_rel,
     }, index=date_keys)
 
 plt.plot(main_df["portfolio_vals"])
@@ -38,5 +41,6 @@ plt.plot(main_df["bench_vals"])
 plt.ylabel('some numbers')
 plt.show()
 
-print(main_df)
+print(np.var(main_df["portfolio_vals"]))
+print(np.var(main_df["bench_vals"]))
 
