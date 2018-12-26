f3aaf341e4d8f7fc1f0906bd9abf65b965e35196
= <tim.schaefer24@hotmail.de>
2018-12-26 23:06:02 +0100

    Process relative values

--- plotting_data.py
+++ plotting_data.py
@@ -23,24 +23,37 @@ dict_keys(['id', 'caption', 'benchmark_name', 'historicalData', 'historicalDataR
 print(y[0]["historicalDataRelative"][1])
 
 portfolio_vals = [val["portfolio_return"] for val in y[0]["historicalData"]]
-portfolio_vals_rel = [val["portfolio_return"] for val in y[0]["historicalDataRelative"]]
 bench_vals = [val["benchmark_return"] for val in y[0]["historicalData"]]
-bench_vals_rel = [val["benchmark_return"] for val in y[0]["historicalDataRelative"]]
 date_keys = [datetime.datetime(val["date_year"], val["date_month"], 1) for val in y[0]["historicalData"]]
 
+bench_vals_rel = [val["benchmark_return"] for val in y[0]["historicalDataRelative"]]
+portfolio_vals_rel = [val["portfolio_return"] for val in y[0]["historicalDataRelative"]]
+rels_key = [datetime.datetime(val["date_year"], val["date_month"], 1) for val in y[0]["historicalDataRelative"]]
+
+# remove all after 2017, 10, 1, 0, 0) ?
 
 main_df = pd.DataFrame(
     {'portfolio_vals': portfolio_vals,
-     #'portfolio_vals_rel' : portfolio_vals_rel,
      'bench_vals': bench_vals,
-    # 'bench_vals_rel': bench_vals_rel,
     }, index=date_keys)
+# TODO refactor and calc vola
+rel_DF = pd.DataFrame(
+    {'portfolio_vals_rel' : portfolio_vals_rel,
+    'bench_vals_rel': bench_vals_rel,
+    }, index=rels_key)
+
+main_df2 = rel_DF.join(main_df, how='left')
+
+print(len(rel_DF))
+print(len(main_df))
+print(len(main_df2))
+
 
 plt.plot(main_df["portfolio_vals"])
 plt.plot(main_df["bench_vals"])
 plt.ylabel('some numbers')
 plt.show()
 
-print(np.var(main_df["portfolio_vals"]))
-print(np.var(main_df["bench_vals"]))
+print(np.var(main_df2["portfolio_vals_rel"]))
+print(np.var(main_df2["bench_vals_rel"]))
 
